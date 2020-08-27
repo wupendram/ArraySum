0abd74265f11b6e60ed5d814591c097dd7bbd7f5
Sree Wupendram <sree@wupendram.com>
2020-08-27 09:52:54 +0100

    code

---
+++ ArraySum/ArraySum.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArraySum
+{
+    public class ArraySum
+    {
+        public int GetIndexFromArray(int[] array)
+        {
+            List<int> leftList = new List<int>();
+            List<int> rightList = new List<int>(array);
+            
+            int index = -1;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                int leftSum = 0;
+                int rightSum = 0;
+
+                leftList.Add(array[i]);
+                rightList.Remove(array[i]);
+
+                for (int j = 0; j < leftList.Count; j++)
+                {
+                    leftSum += leftList[j];
+                }
+
+                for (int k = 0; k < rightList.Count; k++)
+                {
+                    rightSum += rightList[k];
+                }
+                
+                if (leftSum == rightSum)
+                {
+                    index = i; 
+                }
+            }
+            return index;
+        }
+    }
+}
---
+++ ArraySum/Tests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace ArraySum
+{
+    [TestFixture]
+    public class Tests
+    {
+        [TestCase(new int[] { 2, 3, 5, 9, 1 }, 2)]
+        [TestCase(new int[] { 1, 2, 3, 5, 9, -8 }, 2)]
+        [TestCase(new int[] { 0, 5, 5}, 1)]
+        [TestCase(new int[] { 2, -3, -5, 9, 3 }, 3)]
+        [TestCase(new int[] { 2, 0, 2 }, 1)]
+        public void GetIndex(int[] array, int expectedIndex)
+        {
+            //int[] array = { 2, 3, 5, 9, 1 };
+            ArraySum arraySum = new ArraySum();
+            int index = arraySum.GetIndexFromArray(array);
+            Assert.AreEqual(expectedIndex, index);
+        }
+    }
+}
